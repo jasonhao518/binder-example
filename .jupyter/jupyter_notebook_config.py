90111ff1d9ca61f4d843f9fe883eb80fe5167629
Yanfeng Hao <jasonhao518@gmail.com>
2022-09-24 22:27:15 +0200

    asdfasdf

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,5 +1,7 @@
 from s3contents import S3ContentsManager
 
+# Tell Jupyter to use S3ContentsManager
+c.ServerApp.contents_manager_class = S3ContentsManager
 c.S3ContentsManager.access_key_id = "zE2qJ1VCy6QnDHRG"
 c.S3ContentsManager.secret_access_key = "XlEV2kRRs2Kel0JAPUZbSb01FCEVWt42"
 c.S3ContentsManager.endpoint_url = "https://minio.findi.io"
