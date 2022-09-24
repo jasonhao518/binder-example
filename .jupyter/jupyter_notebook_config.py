0249186bd664b81c276772c898dd308119ecea76
Yanfeng Hao <jasonhao518@gmail.com>
2022-09-24 22:23:15 +0200

    asdfas

---
+++ .jupyter/jupyter_notebook_config.py
@@ -0,0 +1,7 @@
+from s3contents import S3ContentsManager
+
+c.S3ContentsManager.access_key_id = "zE2qJ1VCy6QnDHRG"
+c.S3ContentsManager.secret_access_key = "XlEV2kRRs2Kel0JAPUZbSb01FCEVWt42"
+c.S3ContentsManager.endpoint_url = "https://minio.findi.io"
+c.S3ContentsManager.bucket = "notebook"
+c.S3ContentsManager.prefix = "notebooks/test"
