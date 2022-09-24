c61b496e1c0000feb9688fe5e3c7b95a2df0c2f5
Yanfeng Hao <jasonhao518@gmail.com>
2022-09-24 22:40:28 +0200

    asdfasd

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -2,8 +2,8 @@ from s3contents import S3ContentsManager
 
 # Tell Jupyter to use S3ContentsManager
 c.ServerApp.contents_manager_class = S3ContentsManager
-c.S3ContentsManager.access_key_id = "zE2qJ1VCy6QnDHRG"
-c.S3ContentsManager.secret_access_key = "XlEV2kRRs2Kel0JAPUZbSb01FCEVWt42"
+c.S3ContentsManager.access_key_id = "obEfZT1z6wV5W6vH"
+c.S3ContentsManager.secret_access_key = "j20r5NujjTQz9iFl3XJMAgAphkco30ez"
 c.S3ContentsManager.endpoint_url = "http://data-minio.data.svc.cluster.local:9000"
 c.S3ContentsManager.bucket = "notebook"
 c.S3ContentsManager.prefix = "notebooks/test"
