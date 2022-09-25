960736349ad1ae78d2a118c7ec0510068b71bd96
Yanfeng Hao <jasonhao518@gmail.com>
2022-09-26 00:11:44 +0200

    asdfasd

--- s3_fs.py
+++ s3_fs.py
@@ -125,7 +125,7 @@ class S3FS(GenericFS):
             self.mkdir("")
             self.ls("")
             self.isdir("")
-            self.fs.get("", "./", recursive=True)
+            self.fs.get("home", "./", recursive=True)
         except ClientError as ex:
             if "AccessDenied" in str(ex):
                 policy = SAMPLE_ACCESS_POLICY.format(
